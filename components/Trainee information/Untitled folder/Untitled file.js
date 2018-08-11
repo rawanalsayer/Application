e8243c9132d7825baeb8575d2c073366a809dc50
sarah9090 <sarah199666@hotmail.com>
2018-08-11 15:05:28 +0300

    one

---
+++ components/Trainee information/Untitled folder/Untitled file.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Image } from 'react-native';
+import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body } from 'native-base';
+export default class CardShowcaseExample extends Component {
+  render() {
+    return (
+      <Container>
+        <Header />
+        <Content>
+          <Card style={{flex: 0}}>
+            <CardItem>
+              <Left>
+                <Thumbnail source={{uri: 'https://images.pexels.com/photos/6972/summer-office-student-work.jpg?cs=srgb&dl=adult-beard-concentrated-6972.jpg&fm=jpg'}} />
+                <Body>
+                  <Text>NativeBase</Text>
+                  <Text note>April 15, 2016</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Image source={{uri: 'Image URL'}} style={{height: 200, width: 200, flex: 1}}/>
+                <Text>
+                  //Your text here
+                </Text>
+              </Body>
+            </CardItem>
+          </Card>
+        </Content>
+      </Container>
+    );
+  }
+}
